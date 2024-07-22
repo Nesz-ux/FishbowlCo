506ff905eae56eb6efcbc9de3f358e0671fafdaa
Nesz-ux <nhernandez1902@gmail.com>
2024-07-21 23:10:21 -0600

    Navegacion y login tabs de aplicacion

---
+++ assets/styles/loginStyle.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet } from "react-native";
+
+export const loginStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    color: '#00086a',
+    marginTop: 100,
+    fontSize: 25,
+    textAlign: "center",
+    height: 70,
+    fontWeight:"900",
+    textTransform:"uppercase"
+},
+  logo:{
+    alignSelf:"center"
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignContent:"flex-end",
+    borderWidth: 1,
+    borderColor:"#00086a",
+    borderRadius:25,
+    padding:10,
+    width: 300,
+    marginBottom: 20
+  },
+  input: {
+    marginTop:4,
+  },
+  icon: {
+    marginRight:10,
+  },
+  btnSesion: {
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:25,
+    elevation:3,
+    backgroundColor:'#4246b8',
+    paddingVertical:15,
+    width:300,
+    marginTop: 15,
+    marginBottom:80
+  },
+  textBtn:{
+    fontSize: 20,
+    fontWeight:'500',
+    color:'#FFF',
+  },
+
+
+});
+
+/*
+Azul Fuerte Titulos
+"#00003E"
+Texto
+"#424d98"
+Fondo
+""
+*/
