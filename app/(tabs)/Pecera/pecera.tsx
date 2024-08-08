9d9d774142094982d8a3a8cf21314e151e888f7a
Nesz-ux <nhernandez1902@gmail.com>
2024-08-07 20:45:45 -0600

    Vista pecera, boton de alimentar

--- app/(tabs)/Pecera/pecera.tsx
+++ app/(tabs)/Pecera/pecera.tsx
@@ -1,45 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import axios from 'axios';
-import {esp32Ip} from '../../../config.js'
-
+import React from "react";
+import { StyleSheet, Text, View, Button, Image, Pressable } from "react-native";
+import axios from "axios";
+import { esp32Ip } from "../../../config.js";
+import { peceraStyle } from "../../../assets/styles/peceraStyle";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Pecera() {
   const handleLEDOn = async () => {
     try {
       await axios.get(`${esp32Ip}/led/on`);
-      alert('LED encendido');
+      alert("LED encendido");
     } catch (error) {
       console.error(error);
-      alert('Error encendiendo el LED');
+      alert("Error encendiendo el LED");
     }
   };
 
   const handleLEDOff = async () => {
     try {
       await axios.get(`${esp32Ip}/led/off`);
-      alert('LED apagado');
+      alert("LED apagado");
     } catch (error) {
       console.error(error);
-      alert('Error apagando el LED');
+      alert("Error apagando el LED");
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Control del ESP32</Text>
+    <View style={peceraStyle.container}>
+      <View style={peceraStyle.titlecontainer}>
+        <Text style={peceraStyle.titulo}>Mi Pecera </Text>
+        <MaterialCommunityIcons 
+          name="fishbowl-outline" 
+          size={35} 
+          color="#00086a"
+        />
+      </View>
+
+      <View style={peceraStyle.buttonContainer}>
+        <Image
+          source={require("../../../assets/images/comida-para-pez.png")}
+          style={peceraStyle.image}
+        />
+        <Pressable 
+          style={peceraStyle.button}
+          onPress={handleLEDOn}
+        >
+        <Text style={peceraStyle.textButton}>Alimentar</Text>
+        </Pressable>
+      </View>
       <Button title="Encender LED" onPress={handleLEDOn} />
       <Button title="Apagar LED" onPress={handleLEDOff} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
+//icon: <a href="https://www.flaticon.es/iconos-gratis/comida-para-pez" title="comida-para-pez iconos">Comida-para-pez iconos creados por Freepik - Flaticon</a>
